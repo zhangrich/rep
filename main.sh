46be8868ca5702f61afdbb28372f23e9c0376591
zhangrich <50880179+zhangrich@users.noreply.github.com>
2023-04-23 10:07:44 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,7 @@
+# rm -rf alist* #Uncomment this line to update
+if [ ! -f "alist" ];then
+  curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
+  tar -zxvf alist.tar.gz
+  rm -f alist.tar.gz
+fi
+./alist server --no-prefix
